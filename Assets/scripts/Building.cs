83fb87d0665eac1bbf9cb6fdc852b4b7e39c5931
Devon Plouffe <devon.plouffe@carleton.ca>
2017-05-29 11:46:48 -0400

    misc additions

--- Assets/scripts/Building.cs
+++ Assets/scripts/Building.cs
@@ -4,13 +4,38 @@ using UnityEngine;
 
 public class Building : MonoBehaviour {
 
+  private int storageCapacity;
+  private int currentStorageCount;
+  private int civilianCapacity;
+  private int currentCivilianCount;
+
+
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+  {
+
 	}
-	
+
 	// Update is called once per frame
-	void Update () {
-		
+	void Update ()
+  {
+
 	}
+  void setCivilianCapacity(int cap)
+  {
+    this.civilianCapacity = cap;
+  }
+  int getCivilianCapacity()
+  {
+    return civilianCapacity;
+  }
+
+  void setStorageCapacity(int cap)
+  {
+    this.storageCapacity = cap;
+  }
+  int getStorageCapacity()
+  {
+    return storageCapacity;
+  }
 }
