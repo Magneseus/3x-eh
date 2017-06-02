b465e82391811e62dcb6de04ea78c8185e3c7d7e
Justin Zhang <xuelizhang@cmail.carleton.ca>
2017-06-02 13:13:35 -0400

    City View UI, skip other view UI first

---
+++ 3X eh!/Assets/Scripts/CityViewMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CityViewMenu : MonoBehaviour {
+
+    public GameObject menu;
+    public GameObject buildingPane;
+
+	// Use this for initialization
+	void Start () {
+        menu.SetActive(false);
+        buildingPane.SetActive(false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void ShowMenu()
+    {
+        menu.SetActive(true);
+    }
+
+    public void HideMenu()
+    {
+        menu.SetActive(false);
+    }
+    public void ShowBuilding()
+    {
+        buildingPane.SetActive(true);
+    }
+
+    public void HideBuilding()
+    {
+        buildingPane.SetActive(false);
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Region");
+    }
+}
