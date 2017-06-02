b080cbb54b21dfebd52e91cbc5b6d031835e7fae
Matt Mayer <matt.bmayer@gmail.com>
2017-06-02 03:43:21 -0400

    Added some variables to represent a Resource.
    ID - unique int
    Name - obvs
    Value - how much of this resource is here

--- Assets/scripts/Resource.cs
+++ Assets/scripts/Resource.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Resource : MonoBehaviour, TurnUpdatable
 {
+    public int    ID;
+    public string Name;
+    public float  Value;
 
 	// Use this for initialization
 	void Start () {
