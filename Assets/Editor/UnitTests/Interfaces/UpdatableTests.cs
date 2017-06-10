0e470b3807e485ad94b345b6d9421cb6b097b89e
yannickrobert <yannick.robert@carleton.ca>
2017-06-09 20:13:36 -0400

    added unit tests for TurnUpdatable

---
+++ Assets/Editor/UnitTests/Interfaces/UpdatableTests.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+using System;
+
+public class UpdatableTests {
+
+	[Test]
+	public void UpdatableTestsSimplePasses() {
+        // Use the Assert class to test conditions.
+        Assert.Pass();
+	}
+
+	[Test]
+    public void CanBeInstantiated()
+    {
+        int startNumber = 0;
+        int numDaysPassed = 7;
+
+        StubUpdatable myStub = new StubUpdatable(startNumber);
+        Assert.AreEqual(startNumber, myStub._number);
+
+        myStub.TurnUpdate(numDaysPassed);
+        Assert.AreEqual(numDaysPassed, myStub._number);
+    }
+
+    class StubUpdatable : TurnUpdatable
+    {
+        public int _number;
+
+        public StubUpdatable(int number)
+        {
+            _number = number;
+        }
+
+        public void TurnUpdate(int numDaysPassed)
+        {
+            _number += numDaysPassed;
+        }
+    }
+
+
+}
