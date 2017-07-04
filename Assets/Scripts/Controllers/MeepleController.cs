18d92ca031d15db302dbcda43a2b441781bb60dc
Matt Mayer <matt.bmayer@gmail.com>
2017-07-04 13:34:31 -0400

    Added the basic MeepleController script/prefab.

---
+++ Assets/Scripts/Controllers/MeepleController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeepleController : MonoBehaviour {
+
+    public DPerson dPerson;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    internal void ConnectToDataEngine(DGame dGame, string cityName)
+    {
+        dPerson= new DPerson(dGame.Cities[cityName], this);
+    }
+}
