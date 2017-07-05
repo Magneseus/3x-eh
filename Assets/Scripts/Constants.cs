3279deb3d320d989be70b6b7f3a343227f4c4542
Matt Mayer <matt.bmayer@gmail.com>
2017-07-05 19:53:34 -0400

    Finished the Trays for Tasks on buildings.
    - can drag meeples to and from task trays
    - trays are generated automatically based on the max number of people
    who can be allocated to that task

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -6,6 +6,9 @@ public static class Constants {
 
     public static string OTTAWA_PREFAB_PATH = @"Prefabs/Cities/Ottawa";
     public static string BUILDING_PREFAB_PATH = @"Prefabs/Buildings/Building";
+    public static string TASK_TRAY_PREFAB_PATH = @"Prefabs/Tasks/TaskTray";
+    public static string TASK_TRAY_SINGLE_PREFAB_PATH = @"Prefabs/Tasks/TaskTraySingle";
+    public static string MEEPLE_PREFAB_PATH = @"Prefabs/MeepleController";
 
     public static string OTTAWA_JSON_PATH = @"Data/Ottawa";
 }
--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -5,7 +5,6 @@ using SimpleJSON;
 
 public class GameController : MonoBehaviour
 {
-
     public DGame dGame = new DGame();
 
 
@@ -30,18 +29,23 @@ public class GameController : MonoBehaviour
         CityController cityController = InstantiatePrefab<CityController>(Constants.OTTAWA_PREFAB_PATH);        
         cityController.ConnectToDataEngine(dGame, cityJson["name"]);     
         
+        // Load in all buildings for the city
         foreach(JSONNode building in cityJson["buildings"].AsArray)
         {
+            // Get the random positions available
             JSONNode xPos = building["position"]["x"].AsArray;
             JSONNode yPos = building["position"]["y"].AsArray;
 
             BuildingController bControl = CreateBuilding(cityJson["name"], building["name"], new Vector3(Random.Range(xPos[0], xPos[1]), Random.Range(yPos[0], yPos[1]), 1));
 
+            // Load in all the tasks for this building
             foreach (JSONNode task in building["tasks"].AsArray)
             {
+                // TODO: Check for "special" tasks, like assess/explore/etc.
                 string taskName = task["name"];
                 int maxPeople = task["maxPeople"];
 
+                // Load in the resource output for this task
                 DResource taskResource = DResource.Create(
                     task["resource"]["name"],
                     task["resource"]["amount"]);
@@ -51,8 +55,14 @@ public class GameController : MonoBehaviour
                     taskResource, 
                     maxPeople, 
                     taskName);
+
+                // Generate the task controller and attach it
+                TaskController newTaskController = AttachTaskController(newTask, bControl);
             }
         }
+
+        //TODO: Remove this
+        CreateMeeple(cityJson["name"]);
 
         return cityController;
     }
@@ -67,8 +77,34 @@ public class GameController : MonoBehaviour
         return buildingController;
     }
 
+    // TODO: Spawn in "empty building"
+    public MeepleController CreateMeeple(string cityName)
+    {
+        MeepleController meepleController = InstantiatePrefab<MeepleController>(Constants.MEEPLE_PREFAB_PATH);
+        meepleController.ConnectToDataEngine(dGame, cityName);
+
+        return meepleController;
+    }
+
+    public TaskController AttachTaskController(DTask dTask, BuildingController buildingController)
+    {
+        TaskController taskController = InstantiatePrefab<TaskController>(Constants.TASK_TRAY_PREFAB_PATH, buildingController.transform);
+        taskController.ConnectToDataEngine(dTask);
+
+        // Give BuildingController a reference to it and vice versa
+        buildingController.AddTaskController(taskController);
+        taskController.buildingController = buildingController;
+
+        return taskController;
+    }
+
     private T InstantiatePrefab<T>(string prefabPath)
     {
         return (Instantiate(Resources.Load(prefabPath)) as GameObject).GetComponent<T>();
+    }
+
+    private T InstantiatePrefab<T>(string prefabPath, Transform parent)
+    {
+        return (Instantiate(Resources.Load(prefabPath), parent) as GameObject).GetComponent<T>();
     }
 }
