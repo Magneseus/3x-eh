c11e38bbe17e1df74cd3a612cf4e166fb149b593
Matt Mayer <matt.bmayer@gmail.com>
2017-07-12 19:34:36 -0400

    Fixed EndTurn button missing event handler.

---
+++ Assets/Scripts/GUI/CityViewUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CityViewUI : MonoBehaviour
+{
+    private GameController gameController;
+    private Button endTurnButton;
+
+	// Use this for initialization
+	void Start ()
+    {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+
+        // Set the end turn button's OnClick
+        foreach (var button in GetComponentsInChildren<Button>())
+        {
+            if (button.name == "EndTurnButton")
+            {
+                endTurnButton = button;
+            }
+        }
+        endTurnButton.onClick.AddListener(gameController.dGame.EndTurnUpdate);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
