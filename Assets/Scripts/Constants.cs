ae77ff323ab331cce490393dfefb4f0b44a6aa95
Devon Plouffe <devon.plouffe@carleton.ca>
2017-07-20 16:30:19 -0400

    console output for meeple infection level [testing]

--- Assets/Scripts/Constants.cs
+++ Assets/Scripts/Constants.cs
@@ -33,7 +33,7 @@ public static class Constants {
     #region Merson Infection
     public static int MERSON_INFECTION_MIN = 0;
     public static int MERSON_INFECTION_MAX = 2;
-
+    public static float MERSON_INFECTION_PROBABILITY = 0.05f;
     public static float MERSON_INFECTION_TASK_MODIFIER = 0.5f;
     #endregion
 }
