93b71b8cd44c5e74129a9d8383dfae365c7a066c
Jater Xu <sorceresslibra@icloud.com>
2017-07-22 13:33:15 -0400

    UI Prefab and Scripts

---
+++ Assets/Scripts/GUI/Sprint6/ResourceManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceManager : MonoBehaviour {
+
+    public Text[] resources = new Text[6];
+    public string[] resNames = new string[6];
+    
+    public int[] res = { 200, 500, 300, 800, 1000, 200 };
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        resources[0].text = resNames[0] + " : " + res[0];
+
+        resources[1].text = resNames[1] + " : " + res[1];
+
+        resources[2].text = resNames[2] + " : " + res[2];
+
+        resources[3].text = resNames[3] + " : " + res[3];
+
+        resources[4].text = resNames[4] + " : " + res[4];
+
+        resources[5].text = resNames[5] + " : " + res[5];
+
+    }
+
+}
