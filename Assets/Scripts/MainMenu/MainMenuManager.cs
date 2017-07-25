d7387a2349fb1fc9244765c6baae999b973da003
Jater Xu <sorceresslibra@icloud.com>
2017-07-25 17:15:08 -0400

    added menu scripts

---
+++ Assets/Scripts/MainMenu/MainMenuManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour {
+
+    public GameObject loadingSceneManager;
+    public GameObject camControl;
+    public Transform newGameMount;
+    public Transform mainMenuMount;
+
+    public Button newGameBtn;
+    public Button loadBtn;
+    public Button optBtn;
+    public Button creditBtn;
+    public Button quitBtn;
+
+    public float distance = 10f;
+
+	// Use this for initialization
+	void Start () {
+        loadingSceneManager.GetComponent<LoadingSceneManager>().Fade(false, 3f);
+        newGameBtn.interactable = false;
+        loadBtn.interactable = false;
+        optBtn.interactable = false;
+        creditBtn.interactable = false;
+        quitBtn.interactable = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Vector3.SqrMagnitude(camControl.transform.position - newGameMount.position) < distance)
+        {
+            StartCoroutine(LoadingGame());
+        }
+        if(Vector3.SqrMagnitude(camControl.transform.position - mainMenuMount.position) <= distance && (!newGameBtn.interactable && !loadBtn.interactable && !optBtn.interactable && !creditBtn.interactable && !quitBtn.interactable))
+        {
+            
+            newGameBtn.interactable = true;
+            loadBtn.interactable = true;
+            optBtn.interactable = true;
+            creditBtn.interactable = true;
+            quitBtn.interactable = true;
+        }
+        if(Vector3.SqrMagnitude(camControl.transform.position - mainMenuMount.position) > distance && (newGameBtn.interactable && loadBtn.interactable && optBtn.interactable && creditBtn.interactable && quitBtn.interactable))
+        {
+            newGameBtn.interactable = false;
+            loadBtn.interactable = false;
+            optBtn.interactable = false;
+            creditBtn.interactable = false;
+            quitBtn.interactable = false;
+
+        }
+	}
+
+    IEnumerator LoadingGame()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        loadingSceneManager.GetComponent<LoadingSceneManager>().Fade(true, 1f);
+        yield return new WaitForSeconds(10f);
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Game");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/MainMenu/PanelShaking.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelShaking : MonoBehaviour {
+
+	public Vector3 origin;
+	Vector3 target;
+	Quaternion targetRot;
+	public Quaternion originalRot;
+	public float ratio;
+	public float ratioMultiplier;
+
+	void Start(){
+		origin = this.transform.position;
+		originalRot = this.transform.rotation;
+	}
+	void Update(){
+		float movement = Mathf.Sin (Time.time);
+		//target = new Vector3(origin.x+movement/2,origin.y+movement/2,origin.z+movement/10);
+		targetRot = new Quaternion (originalRot.x+movement*Time.deltaTime,originalRot.y+movement*Time.deltaTime,originalRot.z+movement*Time.deltaTime,originalRot.w+movement*Time.deltaTime);
+		//Debug.Log (movement);	
+		//transform.position = Vector3.Lerp (transform.position,target,ratio);
+		this.transform.rotation = Quaternion.Lerp (this.transform.rotation,targetRot,ratio*ratioMultiplier);
+
+	}
+}
+
