0aca48534aba24e56a724d1d9a8998c7c912e8b5
Jater Xu <shiraikuroko@vip.qq.com>
2017-08-13 02:19:49 -0400

    Audio Mastering & BGM Intergration

--- Assets/Scripts/SoundController.cs
+++ Assets/Scripts/SoundController.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class SoundController : MonoBehaviour {
 
+	public GameObject bgmLibrary;
+	public GameObject sfxLibrary;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButton(0)){
-			this.GetComponents<AudioSource>()[0].Play();
+			sfxLibrary.GetComponents<AudioSource>()[0].Play();
+		}
+		if (GameObject.Find("City(Clone)") != null){
+			if (GameObject.Find("City(Clone)").GetComponent<SpriteRenderer>().sprite.name == "ottawa"){
+				if (GameObject.Find("BGMAudio") != null){
+					GameObject.Find ("BGMAudio").GetComponent<AudioSource> ().clip = bgmLibrary.GetComponents<AudioSource>()[0].clip;
+					//GameObject.Find ("BGMAudio").GetComponent<AudioSource> ().Play ();
+				}
+			}
+			if (GameObject.Find("City(Clone)").GetComponent<SpriteRenderer>().sprite.name == "iqaluit"){
+				if (GameObject.Find("BGMAudio") != null){
+					GameObject.Find ("BGMAudio").GetComponent<AudioSource> ().clip = bgmLibrary.GetComponents<AudioSource>()[1].clip;
+					//GameObject.Find ("BGMAudio").GetComponent<AudioSource> ().Play ();
+				}
+			}
+			if (GameObject.Find("City(Clone)").GetComponent<SpriteRenderer>().sprite.name == "vancouver"){
+				if (GameObject.Find("BGMAudio") != null){
+					GameObject.Find ("BGMAudio").GetComponent<AudioSource> ().clip = bgmLibrary.GetComponents<AudioSource>()[2].clip;
+					//GameObject.Find ("BGMAudio").GetComponent<AudioSource> ().Play ();
+				}
+			}
 		}
 	}
 }
