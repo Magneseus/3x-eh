4c2d5169047f90aa2f55f241c9ac8c10e6a277d8
Justin Zhang <xuelizhang@cmail.carleton.ca>
2017-08-13 21:06:09 -0400

    Add Continue feature

---
+++ Assets/Scripts/GUI/ContinueGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinueGame : MonoBehaviour {
+
+    private GameController gameController;
+
+	// Use this for initialization
+	void Start () {
+        gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Continue()
+    {
+        gameController.LoadGame("Continue.json");
+    }
+}
