df8df771aeed20bf8b98493349afff9e3586021c
Justin Zhang <xuelizhang@cmail.carleton.ca>
2017-08-13 22:59:45 -0400

    Continue Features added both UI and hook up to engine

---
+++ Assets/Scripts/GUI/ContinueGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinueGame : MonoBehaviour {
+
+    private GameController gameContoller;
+    public string contiuneFileName;
+
+	// Use this for initialization
+	void Start () {
+        gameContoller = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void Continue()
+    {
+        gameContoller.LoadGame(contiuneFileName);
+    }
+}
