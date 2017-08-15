664d96cda53f8f0a891a92db3a9db3475fe20f9f
Magneseus <matt.bmayer@gmail.com>
2017-08-15 15:13:44 -0400

    Revert assess changes.

--- Assets/Scripts/Data/SubTasks/DTask_Assess.cs
+++ Assets/Scripts/Data/SubTasks/DTask_Assess.cs
@@ -8,15 +8,11 @@ using SimpleJSON;
 public class DTask_Assess : DTask
 {
     private float assessAmount;
-    private float oAssessAmount;
-    private float falloff;
 
     public DTask_Assess(DBuilding dBuilding, float assessAmount, int dMaxPeople, string dName) : base(dBuilding, null, dMaxPeople, dName, 0.0f)
     {
       //TODO: make this a standard amount
         this.assessAmount = assessAmount;
-        oAssessAmount = assessAmount;
-        falloff = .9f;
 
         ForceClean();
         ForceFixed();
@@ -36,8 +32,6 @@ public class DTask_Assess : DTask
             {
                 float modifier = taskSlot.Person.Infection == Constants.MERSON_INFECTION_MIN ? 1 : Constants.MERSON_INFECTION_TASK_MODIFIER;
                 building.Assess(assessAmount * Constants.MERSON_INFECTION_TASK_MODIFIER);
-
-                assessAmount = Mathf.Max(assessAmount * falloff, oAssessAmount / 16);
             }
         }
     }
